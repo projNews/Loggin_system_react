573da098f54d587d6c5bdc644759481f441eb12e
Jace <aleshko.vitali@gmail.com>
2020-03-15 19:16:22 +0300

    migration

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyle } from './style';
+import LoginForm from './LoginForm';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <LoginForm />
+  </>
+);
+
+export default App;
